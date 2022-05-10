dd032a2ce5878044688bcb170e042de350b48de2
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2022-05-09 23:02:23 -0300

    🔥 remove old animations

--- src/components/Screen/styles.ts
+++ src/components/Screen/styles.ts
@@ -1,6 +1,4 @@
 import styled, { css } from "styled-components";
-import { rotateIcon } from "@animations";
-import { Moon, Sun } from "phosphor-react";
 import { rem } from "polished";
 
 export const Container = styled.div`
@@ -25,43 +23,4 @@ export const Footer = styled.footer`
     font-size: ${rem(15)};
     color: ${theme.colors.text.primary};
   `}
-`;
-
-export const SwitchTheme = styled.button`
-  ${({ theme }) => css`
-    width: ${rem(45)};
-    height: ${rem(45)};
-    background-color: transparent;
-    color: ${theme.colors.negative};
-    cursor: pointer;
-
-    border: none;
-    border-radius: 50%;
-
-    position: fixed;
-    top: ${rem(30)};
-    right: ${rem(30)};
-
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    transition-duration: 0.3s;
-
-    box-shadow: 0 0 ${rem(8)} ${theme.colors.negative};
-
-    &&:hover {
-      background-color: ${theme.colors.negative};
-      color: ${theme.colors.background};
-    }
-  `}
-`;
-
-export const MoonIcon = styled(Moon)`
-  font-size: ${rem(32)};
-  animation: ${rotateIcon} 1s;
-`;
-
-export const SunIcon = styled(Sun)`
-  font-size: ${rem(32)};
-  animation: ${rotateIcon} 1s;
 `;

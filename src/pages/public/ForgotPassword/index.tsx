e7ef2fbb36b8a042ca8fc999e7ff282ecdd72bb4
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2022-05-23 09:43:07 -0300

    ⚡️

--- src/pages/public/ForgotPassword/index.tsx
+++ src/pages/public/ForgotPassword/index.tsx
@@ -1,69 +1,75 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-import { sendPasswordResetEmail } from "firebase/auth";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+// import { sendPasswordResetEmail } from "firebase/auth";
+// import { useForm } from "react-hook-form";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from "react-toastify";
 
-import { Form, Logo, PublicScreen } from "@components";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { auth } from "@services";
-import { forgotPasswordSchema, formatErrorMessage } from "@utils";
+// import { Form, Logo, PublicScreen } from "@components";
+// import { yupResolver } from "@hookform/resolvers/yup";
+// import { auth } from "@services";
+// import { forgotPasswordSchema, formatErrorMessage } from "@utils";
+
+// import * as S from "./styles";
+
+// type FormType = {
+//   email: string;
+// };
+
+// export function ForgotPassword() {
+//   const navigate = useNavigate();
+//   const [isLoading, setIsLoading] = useState(false);
+//   const {
+//     handleSubmit,
+//     formState: { errors },
+//     register,
+//   } = useForm<FormType>({ resolver: yupResolver(forgotPasswordSchema) });
+
+//   useEffect(() => {
+//     if (errors.email?.message)
+//       toast.warn(formatErrorMessage(errors.email.message));
+//   }, [errors]);
+
+//   const sendLinkHandler = async ({ email }: FormType) => {
+//     setIsLoading(true);
+//     try {
+//       await sendPasswordResetEmail(auth, email);
+//       toast.success(
+//         "The email with link to update your password has been sent."
+//       );
+//       navigate(-1);
+//     } catch ({ message }) {
+//       if (message === "Firebase: Error (auth/user-not-found).") {
+//         toast.error("This email doesn`t exists.");
+//         return;
+//       }
+//       toast.error("An error has ocurred. Try it again later.");
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   return (
+//     <PublicScreen>
+//       <S.Container>
+//         <Logo />
+//         <Form
+//           isLoading={isLoading}
+//           autoCapitalize="off"
+//           title="Reset password"
+//           submitText="Send link"
+//           goBack
+//           onSubmit={handleSubmit(sendLinkHandler)}
+//         >
+//           <S.Input placeholder="Email" {...register("email")} />
+//         </Form>
+//       </S.Container>
+//     </PublicScreen>
+//   );
+// }
 
 import * as S from "./styles";
 
-type FormType = {
-  email: string;
-};
-
 export function ForgotPassword() {
-  const navigate = useNavigate();
-  const [isLoading, setIsLoading] = useState(false);
-  const {
-    handleSubmit,
-    formState: { errors },
-    register,
-  } = useForm<FormType>({ resolver: yupResolver(forgotPasswordSchema) });
-
-  useEffect(() => {
-    if (errors.email?.message)
-      toast.warn(formatErrorMessage(errors.email.message));
-  }, [errors]);
-
-  const sendLinkHandler = async ({ email }: FormType) => {
-    setIsLoading(true);
-    try {
-      await sendPasswordResetEmail(auth, email);
-      toast.success(
-        "The email with link to update your password has been sent."
-      );
-      navigate(-1);
-    } catch ({ message }) {
-      if (message === "Firebase: Error (auth/user-not-found).") {
-        toast.error("This email doesn`t exists.");
-        return;
-      }
-      toast.error("An error has ocurred. Try it again later.");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  return (
-    <PublicScreen>
-      <S.Container>
-        <Logo />
-        <Form
-          isLoading={isLoading}
-          autoCapitalize="off"
-          title="Reset password"
-          submitText="Send link"
-          goBack
-          onSubmit={handleSubmit(sendLinkHandler)}
-        >
-          <S.Input placeholder="Email" {...register("email")} />
-        </Form>
-      </S.Container>
-    </PublicScreen>
-  );
+  return <S.Container />;
 }
